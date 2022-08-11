f3be5452d2240f209f3342471c21e7101564ffc4
qp0kycHuk <serega990112@mail.ru>
2022-08-11 12:38:42 +0300

    add all pages

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import tab from 'npm-kit-tab';
 import toggle from 'npm-kit-toggle';
 import ripple from 'npm-kit-ripple';
 import Swiper, { Navigation, Pagination, Scrollbar, Autoplay, Grid, Thumbs, EffectFade, Lazy } from 'swiper';
-
+import inputmask from "./js/inputmask";
+import ymaps from 'ymaps';
 
 
 import 'npm-kit-ripple/index.css';
@@ -31,11 +32,17 @@ function loadHandler() {
 	toggle.init();
 	ripple.init();
 	theme.init();
+	inputmask.init(document)
 
 	ripple.attach('.btn')
 	ripple.attach('.waved')
 	ripple.deAttach('.btn--link')
 
+	document.addEventListener('click', clickHandler);
+
+	window.addEventListener('scroll', mapsInit)
+	document.addEventListener('click', mapsInit)
+	setTimeout(mapsInit, 5000)
 }
 
 window.addEventListener('toggleopen', (event) => {
@@ -51,3 +58,71 @@ window.addEventListener('toggleclose', (event) => {
 	}
 
 })
+
+function clickHandler(event) {
+	function scrollTo() {
+		const target = event.target.closest('[data-scroll]');
+		const href = target.getAttribute('data-scroll');
+
+		if (!href) return;
+		if (href[0] != '#' || href == '#') return;
+
+		event.preventDefault();
+
+		var element = document.querySelector(href);
+		const offset = 45;
+		const bodyRect = document.body.getBoundingClientRect().top;
+		const elementRect = element.getBoundingClientRect().top;
+		const elementPosition = elementRect - bodyRect;
+		const offsetPosition = elementPosition - offset;
+
+		window.scrollTo({
+			top: offsetPosition,
+			behavior: 'smooth'
+		});
+	}
+	if (event.target.closest('[data-scroll]')) scrollTo();
+
+
+
+}
+
+
+let mapInited = false;
+
+function mapsInit() {
+	if (mapInited) return;
+	mapInited = true
+
+	ymaps
+		.load()
+		.then(maps => {
+
+			const map = new maps.Map('map', {
+				center: [45.043069, 38.948660],
+				zoom: 12
+
+			})
+
+			const placemark = new maps.Placemark([45.043069, 38.948660], {}, {
+
+				iconLayout: 'default#image',
+				iconImageHref: '../img/geo.png',
+				iconImageSize: [54, 60],
+				iconImageOffset: [-27, -60]
+
+			})
+
+
+			map.controls.remove('geolocationControl')
+			map.controls.remove('searchControl')
+			map.controls.remove('trafficControl')
+			map.controls.remove('typeSelector')
+			map.controls.remove('fullscreenControl')
+			// map.controls.remove('zoomControl')
+			map.controls.remove('rulerControl')
+			map.behaviors.disable(['scrollZoom'])
+			map.geoObjects.add(placemark)
+		})
+		.catch(error => console.log('Failed to load Yandex Maps', error));
+}
--- src/js/fancybox.js
+++ src/js/fancybox.js
@@ -1,19 +1,37 @@
 import { Fancybox } from "@fancyapps/ui";
 import ru from "@fancyapps/ui/src/Fancybox/l10n/ru"
 import "@fancyapps/ui/dist/fancybox.css";
+import inputmask from "./inputmask";
 
 const init = () => {
   Fancybox.defaults.trapFocus = false
   Fancybox.defaults.autoFocus = false
   Fancybox.defaults.placeFocusBack = false
+  Fancybox.defaults.infinite = false
+  Fancybox.defaults.Thumbs = {
+    autoStart: false,
+  }
   Fancybox.defaults.l10n = ru
   Fancybox.defaults.template.spinner = '<div class="progress progress-circle"> </div>'
 
-  Fancybox.bind('[data-fancybox-modal]', {
+  const customOptions = {
     dragToClose: false,
     mainClass: 'fancybox-custom-modal',
-    
-  })
+    groupAttr: 'data-fancybox-modal',
+    Thumbs: {
+      autoStart: false,
+    },
+    Carousel: {
+
+    },
+    on: {
+      done: (fancybox, slide) => {
+        inputmask.init(slide.$el)
+      }
+    }
+  }
+
+  Fancybox.bind('[data-fancybox-modal]', customOptions)
 
 
   Fancybox.modal = {}
@@ -22,8 +40,7 @@ const init = () => {
       src: src,
       ...options
     }], {
-      dragToClose: false,
-      mainClass: 'fancybox-custom-modal',
+      ...customOptions,
       ...options
     })
   }
